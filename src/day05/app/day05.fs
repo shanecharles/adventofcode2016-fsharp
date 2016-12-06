b426106537f3d2714478a0c9adfd302f3de0a4ed
Shane Charles <shanecharles@burningicesolutions.com>
2016-12-06 10:09:58 -0600

    day 5 part 2

--- src/day05/app/day05.fs
+++ src/day05/app/day05.fs
@@ -3,23 +3,40 @@ module day05
 open System.Text
 let md5 = System.Security.Cryptography.MD5.Create()
 
-let calcHash (data : byte []) = md5.ComputeHash(data)
-
-let calcKeyCode (prefix : string) = 
+let calcHash prefix = string >> ASCIIEncoding.UTF8.GetBytes >> Seq.append prefix
+                      >> Seq.toArray
+                      >> md5.ComputeHash
+let calcKeyCode1 n (prefix : string) = 
     let prefix' = prefix |> ASCIIEncoding.UTF8.GetBytes
-    seq { for i in {0 .. System.Int32.MaxValue} do
-            let code = i |> string |> ASCIIEncoding.UTF8.GetBytes |> Seq.append prefix'
-                        |> Seq.toArray
-                        |> calcHash
+    seq { for i in {n .. System.Int32.MaxValue} do
+            let code = i |> calcHash prefix'
             if code.[0] = 0uy && code.[1] = 0uy && code.[2] < 16uy then
                 yield code.[2] }
+
+let calcKeyCode2 n (prefix : string) = 
+    let prefix' = prefix |> ASCIIEncoding.UTF8.GetBytes
+    seq {
+        let hash = new System.Collections.Generic.HashSet<byte>() 
+        for i in {n .. System.Int32.MaxValue} do
+            let code = i |> calcHash prefix'
+            if code.[0] = 0uy && code.[1] = 0uy && code.[2] < 8uy 
+                && not <| hash.Contains(code.[2]) then
+                    hash.Add(code.[2]) |> ignore
+                    yield (code.[2], code.[3] |> sprintf "%02x" |> (fun s -> s.Substring(0,1))) }
+
 
 let keyCodeToString : byte seq -> string = Seq.map (sprintf "%x") >> System.String.Concat
 
 [<EntryPoint>]
 let main argv =
     let input = argv |> Array.head
-    input |> calcKeyCode |> Seq.take 8 
+    input |> calcKeyCode1 0 |> Seq.take 8 
     |> keyCodeToString 
     |> printfn "Day 5 part 1 result: %s"
+
+    input |> calcKeyCode2 0 |> Seq.take 8
+    |> Seq.sort
+    |> Seq.map snd
+    |> System.String.Concat
+    |> printfn "Day 5 part 2 result: %s"
     0 // return an integer exit code
--- src/day05/tests/day05.tests.fs
+++ src/day05/tests/day05.tests.fs
@@ -4,7 +4,13 @@ open NUnit.Framework
 open day05
 
 [<Test>]
-let ``First key for door id `abc` should be 1`` () =
+let ``Part 2 second key for door id `abc` should be 5`` () =
+    let expected = (1uy, "5")
+    let result = "abc" |> calcKeyCode2 3231929 |> Seq.head
+    Assert.That(result, Is.EqualTo expected)
+
+[<Test>]
+let ``Part 1 first key for door id `abc` should be 1`` () =
     let expected = 1uy
-    let result = "abc" |> calcKeyCode |> Seq.head
+    let result = "abc" |> calcKeyCode1 3231929 |> Seq.head
     Assert.That(result, Is.EqualTo expected)
