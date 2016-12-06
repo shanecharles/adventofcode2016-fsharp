b5b0bd15ef3c5460d09afb271a1967542a5b676f
Shane Charles <shanecharles@burningicesolutions.com>
2016-12-06 09:10:36 -0600

    add a test and format the output

--- src/day05/app/day05.fs
+++ src/day05/app/day05.fs
@@ -6,18 +6,20 @@ let md5 = System.Security.Cryptography.MD5.Create()
 let calcHash (data : byte []) = md5.ComputeHash(data)
 
 let calcKeyCode (prefix : string) = 
-                         let prefix' = prefix |> ASCIIEncoding.UTF8.GetBytes
-                         seq { for i in {0 .. System.Int32.MaxValue} do
-                                let code = i |> string |> ASCIIEncoding.UTF8.GetBytes |> Seq.append prefix'
-                                           |> Seq.toArray
-                                           |> calcHash
-                                if code.[0] = 0uy && code.[1] = 0uy && code.[2] < 16uy then
-                                    yield code.[2] }
+    let prefix' = prefix |> ASCIIEncoding.UTF8.GetBytes
+    seq { for i in {0 .. System.Int32.MaxValue} do
+            let code = i |> string |> ASCIIEncoding.UTF8.GetBytes |> Seq.append prefix'
+                        |> Seq.toArray
+                        |> calcHash
+            if code.[0] = 0uy && code.[1] = 0uy && code.[2] < 16uy then
+                yield code.[2] }
 
+let keyCodeToString : byte seq -> string = Seq.map (sprintf "%x") >> System.String.Concat
 
 [<EntryPoint>]
 let main argv =
     let input = argv |> Array.head
-    input |> calcKeyCode |> Seq.take 8 |> Seq.toArray |> ASCIIEncoding.UTF8.GetString
+    input |> calcKeyCode |> Seq.take 8 
+    |> keyCodeToString 
     |> printfn "Day 5 part 1 result: %s"
     0 // return an integer exit code
--- src/day05/tests/day05.tests.fs
+++ src/day05/tests/day05.tests.fs
@@ -2,3 +2,9 @@ module day05tests
 
 open NUnit.Framework
 open day05
+
+[<Test>]
+let ``First key for door id `abc` should be 1`` () =
+    let expected = 1uy
+    let result = "abc" |> calcKeyCode |> Seq.head
+    Assert.That(result, Is.EqualTo expected)
