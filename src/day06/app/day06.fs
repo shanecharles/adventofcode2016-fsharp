b9f1b79ac99f27bec0ac4c26ee90972401ce75c0
Shane Charles <shanecharles@burningicesolutions.com>
2016-12-06 18:28:44 -0600

    day 6 part 2

--- src/day06/app/day06.fs
+++ src/day06/app/day06.fs
@@ -5,21 +5,28 @@ let addTo (d : Dictionary<int, char list>) i c = match d.ContainsKey(i) with
                                                  | true -> d.[i] <- c :: d.[i]
                                                  | _    -> d.[i] <- [c]
 
-let decodeSignal lines =
+let part1Sorter : (char * char seq) seq ->  (char * char seq) seq = 
+    Seq.sortByDescending (fun (_, cs) -> cs |> Seq.length)
+
+let part2Sorter : (char * char seq) seq ->  (char * char seq) seq = 
+    Seq.sortBy (fun (_, cs) -> cs |> Seq.length)
+
+let decodeSignal sorter lines : char [] =
     let dict = new Dictionary<int, char list>()
     let addTo' = addTo dict
     lines |> Seq.iter (Seq.iteri addTo')
     dict |> Seq.map (fun (kv) -> kv.Key,kv.Value)
     |> Seq.sort
-    |> Seq.map (snd >> Seq.groupBy id >> (Seq.sortByDescending (fun (_, cs) -> cs |> Seq.length) >> Seq.head >> fst))
+    |> Seq.map (snd >> Seq.groupBy id >> sorter >> Seq.head >> fst)
     |> Seq.toArray
-    |> System.String.Concat
-
     
 
 [<EntryPoint>]
 let main argv =
     let file = argv |> Seq.head
     let signals = System.IO.File.ReadAllLines(file)
-    signals |> decodeSignal |> printfn "Day 6 part 1 result: %s"
+    signals |> decodeSignal part1Sorter |> System.String.Concat 
+    |> printfn "Day 6 part 1 result: %s"
+    signals |> decodeSignal part2Sorter |> System.String.Concat 
+    |> printfn "Day 6 part 2 result: %s"
     0 // return an integer exit code
--- src/day06/tests/day06.tests.fs
+++ src/day06/tests/day06.tests.fs
@@ -28,8 +28,13 @@ let ``Sample lines decode of first character should be 'e'`` () =
     Assert.That(result, Is.EqualTo expected)
 
 [<Test>]
-let ``Decoding the sample data should result with 'easter'`` () =
-    let expected = "easter"
-    let result = sampleData |> day06.decodeSignal
+let ``Decoding the sample data with part 1 should result with 'easter'`` () =
+    let expected = "easter" |> Seq.toArray
+    let result = sampleData |> day06.decodeSignal day06.part1Sorter
     Assert.That(result, Is.EqualTo expected)
 
+[<Test>]
+let ``Decoding the sample data with part 2 should result with 'advent'`` () =
+    let expected = "advent" |> Seq.toArray
+    let result = sampleData |> day06.decodeSignal day06.part2Sorter
+    Assert.That(result, Is.EqualTo expected)
