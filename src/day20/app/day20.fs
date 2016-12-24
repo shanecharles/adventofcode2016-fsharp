3454bf5f1d48059400223a133ec174b8edb7a9b1
Shane Charles <shanecharles@burningicesolutions.com>
2016-12-24 16:51:23 -0600

    add day 20 part 2

--- src/day20/app/day20.fs
+++ src/day20/app/day20.fs
@@ -1,6 +1,11 @@
 module day20
 
 let parseLine (l : string) = l.Split('-') |> Array.map int64
+let parseInputFile = 
+    System.IO.File.ReadAllLines 
+    >> Array.map (parseLine >> function 
+                               | [|x;y|] -> (x,y)
+                               | _       -> failwith "invalid input")
 
 let openIps blockedIps =
     let rec nextIp ip blocks = 
@@ -11,14 +16,15 @@ let openIps blockedIps =
                 yield! nextIp (max ip (e + 1L)) t }
     nextIp 0L (blockedIps |> Seq.sortBy fst |> Seq.toList)
 
-let parseInputFile = System.IO.File.ReadAllLines 
-                     >> Array.map (parseLine >> function [|x;y|] -> (x,y)
-                                                       | _       -> failwith "invalid input")
-
 [<EntryPoint>]
 let main argv =
     let input = argv.[0] |> parseInputFile
-    input |> openIps |> Seq.head
+    let availIps = input |> openIps 
+
+    availIps |> Seq.head
     |> printfn "Day 20 part 1 result: %d"
+    
+    availIps |> Seq.length
+    |> printfn "Day 20 part 2 result: %d"
     
     0 // return an integer exit code
